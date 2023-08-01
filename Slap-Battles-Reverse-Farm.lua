a9ef85623acb0fb0636370af44e1214ca394bce9
Lazzy <85132986+RobloxAvatar@users.noreply.github.com>
2023-08-01 19:13:54 +0200

    Update Slap-Battles-Reverse-Farm.lua

--- Slap-Battles-Reverse-Farm.lua
+++ Slap-Battles-Reverse-Farm.lua
@@ -1,22 +1,27 @@
-if game:GetService("Players").LocalPlayer.leaderstats.Glove.Value ~= "Reverse" then return end
-repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-repeat wait() until not game.Players.LocalPlayer.Character:FindFirstChild("entered")
-if not game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
-    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1)
-end
+game:GetService("UserInputService").InputBegan(i,istyping)
+    if istyping == true then return end
+    if i.KeyCode == Enum.KeyCode.LeftAlt then
+        if game:GetService("Players").LocalPlayer.leaderstats.Glove.Value ~= "Reverse" then return end
+        repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+        repeat wait() until not game.Players.LocalPlayer.Character:FindFirstChild("entered")
+        if not game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
+            firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1)
+        end
 
-repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("entered")
-repeat wait() until game.Players[_G.Target].Character:FindFirstChild("entered")
+        repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("entered")
+        repeat wait() until game.Players[_G.Target].Character:FindFirstChild("entered")
 
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(234.469391, -15.7160769, 15.0752077, -0.106970124, 4.20225881e-08, 0.994262218, -2.34862387e-08, 1, -4.47919213e-08, -0.994262218, -2.81428765e-08, -0.106970124)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(234.469391, -15.7160769, 15.0752077, -0.106970124, 4.20225881e-08, 0.994262218, -2.34862387e-08, 1, -4.47919213e-08, -0.994262218, -2.81428765e-08, -0.106970124)
 
-wait(0.5)
+        wait(0.5)
 
-game.ReplicatedStorage.ReverseAbility:FireServer()
+        game.ReplicatedStorage.ReverseAbility:FireServer()
 
-wait(0.5)
+        wait(0.5)
 
-local ohInstance1 = workspace[_G.Target].HumanoidRootPart
+        local ohInstance1 = workspace[_G.Target].HumanoidRootPart
 
-game:GetService("ReplicatedStorage").ReverseHit:FireServer(ohInstance1)
+        game:GetService("ReplicatedStorage").ReverseHit:FireServer(ohInstance1)
+    end
+end)
