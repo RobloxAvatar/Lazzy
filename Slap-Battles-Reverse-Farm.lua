04b9f2ace539bda7a5d8076f6087c00136fdf687
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2023-08-01 18:38:01 +0200

    Create Slap-Battles-Reverse-Farm.lua

---
+++ Slap-Battles-Reverse-Farm.lua
@@ -0,0 +1,21 @@
+if game:GetService("Players").LocalPlayer.leaderstats.Glove.Value ~= "Reverse" then return end
+repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+repeat wait() until not game.Players.LocalPlayer.Character:FindFirstChild("entered")
+wait(1.25)
+if not game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
+    firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1)
+end
+
+repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("entered")
+repeat wait() until game.Players[_G.Target].Character:FindFirstChild("entered")
+
+wait(1)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-57.911137, 362.33728, -0.93506506, -0.0174594577, 6.0804024e-09, 0.999847591, 7.86445256e-08, 1, -7.45462891e-09, -0.999847591, -7.876268888e-08, -0.0174594577)
+
+wait(1.5)
+
+local ohInstance1 = workspace[_G.Target].HumanoidRootPart
+
+game:GetService("ReplicatedStorage").ReverseHit:FireServer(ohInstance1)
