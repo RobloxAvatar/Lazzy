c32a72360f60197760505a08d141180236c73f93
Lazzy <85132986+RobloxAvatar@users.noreply.github.com>
2023-08-01 19:38:29 +0200

    Update Slap-Battles-Reverse-Farm.lua

--- Slap-Battles-Reverse-Farm.lua
+++ Slap-Battles-Reverse-Farm.lua
@@ -4,6 +4,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(i,istyping)
         if game:GetService("Players").LocalPlayer.leaderstats.Glove.Value ~= "Reverse" then return end
         repeat task.wait() until game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
         repeat wait() until not game.Players.LocalPlayer.Character:FindFirstChild("entered")
+        repeat wait() until not game.Players[_G.Target].Character:FindFirstChild("entered")
         if not game.Players.LocalPlayer.Character:FindFirstChild("entered") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
             firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
             firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 1)
@@ -11,7 +12,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(i,istyping)
 
         repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("entered")
         repeat wait() until game.Players[_G.Target].Character:FindFirstChild("entered")
-
+        
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(234.469391, -15.7160769, 15.0752077, -0.106970124, 4.20225881e-08, 0.994262218, -2.34862387e-08, 1, -4.47919213e-08, -0.994262218, -2.81428765e-08, -0.106970124)
 
         wait(0.5)
