6f86cde1749fddd614105d99717a06cb25b69bc4
Lazzy <85132986+RobloxAvatar@users.noreply.github.com>
2023-08-02 14:28:14 +0200

    Update Slap-Battles.lua

--- Slap-Battles.lua
+++ Slap-Battles.lua
@@ -175,6 +175,13 @@ MainTab:Bind("Kill Aura", Enum.KeyCode.G, function()
                                                                                                 local ohInstance1 = game:GetService("Players")[v.Name].Character.HumanoidRootPart
                                                                                                 game:GetService("ReplicatedStorage").b:FireServer(ohInstance1)
                                                                                             end
+                                                                                        else
+                                                                                            if game:GetService("Players").LocalPlayer.leaderstats.Glove.Value == "Thor" then
+                                                                                                for i,v in pairs(game:GetService("Players"):GetChildren()) do
+                                                                                                    local ohInstance1 = game:GetService("Players")[v.Name].Character.HumanoidRootPart
+                                                                                                    game:GetService("ReplicatedStorage").ThorHit:FireServer(ohInstance1)
+                                                                                                end
+                                                                                            end
                                                                                         end
                                                                                     end
                                                                                 end
