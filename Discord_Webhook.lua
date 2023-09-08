a47f8e545878869be2040a9b1f7ac322f483ab16
Lazzy <85132986+RobloxAvatar@users.noreply.github.com>
2023-09-08 11:04:15 +0200

    Update Discord_Webhook.lua

--- Discord_Webhook.lua
+++ Discord_Webhook.lua
@@ -1,36 +1,63 @@
-_G.Webhook = _G.Webhook or ""
-_G.Content = _G.Content or ""
-_G.Username = _G.Username or ""
-_G.AvatarUrl = _G.AvatarUrl or ""
+local Lazzy = {}
 
-local webhookUrl = "https://discord.com/api/webhooks/" .. _G.Webhook
+function Lazzy:CreateNotification(webhook, content, username, avatarurl)
+    local webhookUrl
 
-local message = {
-    content = _G.Content,
-    username = _G.Username,
-    avatar_url = _G.AvatarUrl,
-}
-
-local messageJson = game:GetService("HttpService"):JSONEncode(message)
-
-local requestBody = {
-    Url = webhookUrl,
-    Method = "POST",
-    Headers = {
-        ["Content-Type"] = "application/json",
-        ["Content-Length"] = tostring(#messageJson),
-    },
-    Body = messageJson,
-}
-
-local response = http.request(requestBody)
-
-if response.Success and response.StatusCode == 204 then
-    print("Request was successful")
-else
-    if (response.StatusMessage == "Not Found") then
-        print("Error: Invalid Webhook Link!")
+    if (string.find(webhook, "https://discord.com/api/webhooks/")) then
+        webhookUrl = webhook
     else
-        print("Error Unknown!")
+        webhookUrl = "https://discord.com/api/webhooks/" .. webhook
+    end
+
+    if (webhook == "" or webhook == nil) then
+        print("Please Enter A Webhook!")
+        return
+    end
+
+    if (content == "" or content == nil) then
+        print("Please Enter Content Message!")
+        return
+    end
+
+    if (username == "" or username == nil) then
+        print("Please Enter A Username For The Bot!")
+        return
+    end
+
+    if (avatarurl == "" or avatarurl == nil) then
+        print("Please Enter A Avtar Url For The Bot!")
+        return
+    end
+
+    local message = {
+        content = content,
+        username = username,
+        avatar_url = avatarurl,
+    }
+
+    local messageJson = game:GetService("HttpService"):JSONEncode(message)
+
+    local requestBody = {
+        Url = webhookUrl,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["Content-Length"] = tostring(#messageJson),
+        },
+        Body = messageJson,
+    }
+
+    local response = http.request(requestBody)
+
+    if response.Success and response.StatusCode == 204 then
+        print("Request was successful")
+    else
+        if (response.StatusMessage == "Not Found") then
+            print("Error: Invalid Webhook Link!")
+        else
+            print("Error Unknown!")
+        end
     end
 end
+
+return Lazzy
