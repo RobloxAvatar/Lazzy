e498a53dac517dcbe66525f3ba66a67c4e554183
Lazzy <85132986+RobloxAvatar@users.noreply.github.com>
2023-10-08 13:53:12 +0200

    Create Clipboard.lua

---
+++ Clipboard.lua
@@ -0,0 +1,10 @@
+local Lazzy = {}
+
+function Lazzy.toClipboard(String)
+	local clipBoard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
+	if clipBoard then
+		clipBoard(String)
+	end
+end
+
+return Lazzy
